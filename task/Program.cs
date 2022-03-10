2415151c1a9e9219cc26feed7d72357c14e93e90
Safura234 <safuramg@code.edu.az>
2022-03-10 16:41:44 +0400

    ayin adina gore fesli mueyyenleshdirmek

---
+++ task/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string monthname = Console.ReadLine();
+                // passing string a "str" in
+                // switch statement
+                switch(monthname)
+            {
+                case "december":
+                case "january":
+                case "february":
+                    Console.WriteLine("the season is winter");
+                    break;
+                
+                    
+              
+                 
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+       
+          
+            
+            
+
+
+
+
+
+
+            
+           
+        }
+    }
+}
